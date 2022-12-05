b4c323a170211b4dccc9399a87e528f653df89a3
Jamilly <jamillyksia@gmail.com>
2022-12-05 14:06:02 -0300

    Atualização da página carrinho com função excluir funcionando

--- assets/js/Produtos/main.js
+++ assets/js/Produtos/main.js
@@ -1,58 +1,7 @@
-// bolosJson.map((item) =>{
-//   let boloItem = document.querySelector('.card-produto').cloneNode(true)
-
-//   document.querySelector('.content-produto-section').append(boloItem)
-
-//   boloItem.querySelector('.imagem-produto-card img').src = item.img;
-//   boloItem.querySelector('.nome-produto').innerHTML = item.name;
-//   boloItem.querySelector('.sabor-produto').innerHTML = item.description;
-//   boloItem.querySelector('.preco-produto').innerHTML = `R$ ${item.price.toFixed(2)}`;
-// })
-
-let bolosJson = 
-[ 
-  {
-    id: 0,
-    name: 'Bolo de Chocolate',
-    img: 'assets/img/bolos/bolo1.png',
-    price: 2.00,
-    description: 'Bolo de chocolate com recheio de chocolate',
-    type: 'chocolate',
-    quantidade: 0 
-  },
-  {
-    id: 1,
-    name: 'Bolo de Morango',
-    img: 'assets/img/bolos/bolo2.png',
-    price: 2.00,
-    description: 'Bolo branco com recheio de morango',
-    type: 'morango',
-    quantidade: 0
-  },
-  {
-    id: 2,
-    name: 'Bolo Prestígio',
-    img: 'assets/img/bolos/bolo3.png',
-    price: 2.00,
-    description: 'Bolo de chocolate com recheio de coco',
-    type: 'coco',
-    quantidade: 0
-   },
-  {
-   id: 3,
-   name: 'Bolo Red Velvet',
-   img: 'assets/img/bolos/bolo4.png',
-   price: 2.00,
-   description: 'Bolo de massa vermelha com creme ',
-   type:'morango',
-   quantidade: 0
-  }
-]
-
 inicializarLoja = () => {
   var containerProdutos = document.getElementById('produtos');
   bolosJson.map((val)=>{
-    containerProdutos.innerHTML+= `
+    containerProdutos.innerHTML+= ` 
     
     <section class="content-produto-section">
       <div class="card-produto">
@@ -62,54 +11,61 @@ inicializarLoja = () => {
             <h2 class="nome-produto">`+val.name+`</h2>
             <h3 class="sabor-produto">`+val.description+`</h3>
             <p class="preco-produto">R$`+val.price+`,00</p>
-            <button id="botao-carrinho-produto"><a key="`+val.id+`" href="#">Adicionar ao carrinho</a></button>
+            <a key="`+val.id+`" href="#" id="botao-carrinho-produto">Adicionar ao carrinho</a>
           </div>
         </div>
       </div>
-    `;
+    `; //div para adicionar cada produto
   })
 }
 
 inicializarLoja();
 
-// somaProdutos = () => {
-
-// }
-
 atualizarCarrinho = () => {
-  var containerCarrinho = document.getElementById('carrinho');
+  var containerCarrinho = document.getElementById('carrinho2');
   containerCarrinho.innerHTML = "";
   bolosJson.map((val)=>{
     if(val.quantidade > 0){
     containerCarrinho.innerHTML+= `
     
-    <div class="carrinho">
-    <button class="excluir">X</button>
-    <img src="`+val.img+`" class="imagem-produto-card">
-    <h2>`+val.name+`</h2>
-    <p>quantidade: `+val.quantidade+`+</p>
-    <h3>R$`+val.price+` unid</h3>
-    </div>
-    <hr>
-    <div class="container-total">
-        <h2>VALOR TOTAL</h2>
-        <h3>R$15,87</h3>
+    
+    <div class="carrinho2">
+      <button class="excluir">X</button>
+      <img src="`+val.img+`" class="img-produto-card">
+      <h2>`+val.name+`</h2>
+      <p>Quantidade: `+val.quantidade+`</p>
+      <h3>Preço: R$`+val.price+`,00 </h3>
+      <hr>
     </div>
 
-    <p>`+val.name+` | quantidade: `+val.quantidade+`+</p>
-    <hr></hr>
     `;
     }
   })
+ 
+
 }
 
 var links = document.getElementsByTagName('a');
 
+// soma quantidade
 for(var i = 0; i < links.length; i++){
-  links[i].addEventListener("click",function(){
-    let key = this.getAttribute('key');
+  links[i].addEventListener("click",function(){ //realiza um evento
+    let key = this.getAttribute('key'); //mostra o valor dentro da key
     bolosJson[key].quantidade++;
     atualizarCarrinho();
     return false;
   })
 }
+
+const divCarrinho = document.querySelector("#carrinho2")
+// para apagar o div quando o botão excluir for clicado
+divCarrinho.onclick = function(event){
+  if(event.target.classList.contains('excluir'))
+  {
+    event.target.parentElement.remove() //remove a tag pai do elemento excluir
+    bolosJson.map((val)=>{
+    val.quantidade=0; //mapeia a qtd e dá valor zero
+    })  
+  }
+}
+
